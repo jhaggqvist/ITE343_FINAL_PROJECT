369ba5f03143f100f56df7685d79238d4c45d983
jhaggqvist <jesper.haggqvist@students.stamford.edu>
2017-06-07 19:45:09 +0700

    [jesper]
    Finished login, fully working. We should probably add some image or text ontop to make it look nicer?
    sorted out a bug where when nothing is entered into username and password field and enter is pressed the app shutdown. Added an if statement to fix it.

--- app/src/main/java/com/example/malthael/weatherprediction/RegAndLogin.java
+++ app/src/main/java/com/example/malthael/weatherprediction/RegAndLogin.java
@@ -75,11 +75,19 @@ public class RegAndLogin extends AppCompatActivity {
         String email = getEmail.getText().toString();
         String password = getPassword.getText().toString();
 
-        mAuth.createUserWithEmailAndPassword(email,password);
-        // check if account exists?
+        if (email.equals("") || password.equals("")){
+            Toast.makeText(RegAndLogin.this, "Please enter Email and Password.", Toast.LENGTH_SHORT).show();
+        }
+else {
+            mAuth.createUserWithEmailAndPassword(email,password);
+        }
+
+
+
+
     }
 
-    public void logIn(View view){                               // login with created account
+    public void logIn(final View view){                               // login with created account
         final String email = getEmail.getText().toString();
         String password = getPassword.getText().toString();
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -87,15 +95,24 @@ public class RegAndLogin extends AppCompatActivity {
 
             public void onComplete(@NonNull Task<AuthResult> task) {                // if login is successfull
            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-                Toast.makeText(RegAndLogin.this, "Welcome" + email, Toast.LENGTH_SHORT).show();
 
 
+                if (task.isSuccessful()){
+                    Toast.makeText(RegAndLogin.this, "Welcome" + email, Toast.LENGTH_SHORT).show();
+                  loginSuccess(view); // new intent for MainActivity
+                }
 
                 if(!task.isSuccessful()){                                           // if login is not successfull
-                    Toast.makeText(RegAndLogin.this, "failed to login", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegAndLogin.this, "Wrong email or password", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //error
                 }
             }
         });
+
+
+
 
     }
     @Override
@@ -109,5 +126,9 @@ public class RegAndLogin extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+    public void loginSuccess(View view){
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
     }
 }
