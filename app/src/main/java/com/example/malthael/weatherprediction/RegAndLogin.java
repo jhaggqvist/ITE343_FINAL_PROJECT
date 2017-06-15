4d6e55b25025ca0e93a905c0ab3d209be1c3837a
jhaggqvist <jesper.haggqvist@students.stamford.edu>
2017-06-15 19:29:13 +0700

    [jesper]
    Ok, I think im starting to understand how the gPlus sign in works now however im still stuck, i get error "Local module descriptor class for com.google.firebase.auth not found."

--- app/src/main/java/com/example/malthael/weatherprediction/RegAndLogin.java
+++ app/src/main/java/com/example/malthael/weatherprediction/RegAndLogin.java
@@ -1,30 +1,35 @@
 package com.example.malthael.weatherprediction;
 
 
-import java.lang.String;
-
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class RegAndLogin extends AppCompatActivity {
+public class RegAndLogin extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String TAG = "";
     private EditText getEmail;
     private EditText getPassword;
+    private GoogleApiClient mGoogleApiClient;
+    private static int RC_SIGN_IN = 9001;
 
 
 
@@ -36,8 +41,23 @@ public class RegAndLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_and_login);
+//--------GOOGLE--------
+        //findViewById(R.id.gPlus).setOnClickListener((View.OnClickListener) this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
 
+
+//--------GOOGLE--------
+
+
+        //--------FIREBASE--------
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
 
@@ -71,6 +91,53 @@ public class RegAndLogin extends AppCompatActivity {
     }// end of onCreate
 
 
+    //-----GOOGLE
+
+    /* @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.gPlus:
+                signIn();
+                break;
+        }
+    }
+*/
+public void signInG(){
+    Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+    startActivityForResult(signInIntent, RC_SIGN_IN);
+}
+
+public void onActivityResult(int requestCode, int resultCode, Intent data){
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == RC_SIGN_IN){
+        GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+        handleSignInResult(result);
+    }
+
+}
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+           // mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            //updateUI(true);
+            String name = acct.getDisplayName();
+
+            // pop a welcome toast and send them to weather app
+            Toast.makeText(RegAndLogin.this, "test" + name, Toast.LENGTH_SHORT).show();
+
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+        }
+    }
+    //-----/GOOGLE
+
+//------FIREBASE
     public void createAccount(View view){                       // creates a new account
         String email = getEmail.getText().toString();
         String password = getPassword.getText().toString();
@@ -140,4 +207,10 @@ else {
         Intent myIntent = new Intent(this, MainActivity.class);
         startActivity(myIntent);
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    //-----/FIREBASE
 }
